5b2cb94b9b7203c1f92976ee7d085f6c32dee230
RuseHT <henrik_tran@hotmail.com>
2023-10-12 10:49:15 +0200

    ting

--- libraries/AP_InertialSensor/examples/sensor_test/sensor_test.cpp
+++ libraries/AP_InertialSensor/examples/sensor_test/sensor_test.cpp
@@ -4,14 +4,15 @@
 
 #include <AP_HAL/AP_HAL.h>
 #include <AP_BoardConfig/AP_BoardConfig.h>
-#include <AP_InertialSensor/AP_InertialSensor_Invensense.h>
+#include <AP_InertialSensor/AP_InertialSensor_Invensensev3.h>
+#include <AP_InertialSensor/AP_InertialSensor.h>
 #include <AP_ExternalAHRS/AP_ExternalAHRS.h>
 #include <AP_Logger/AP_Logger.h>
 #include <GCS_MAVLink/GCS_Dummy.h>
 
 const AP_HAL::HAL &hal = AP_HAL::get_HAL();
 
-static AP_InertialSensor_Invensense ins;
+AP_InertialSensor_Invensensev3 ins(AP_InertialSensor &imu, hal.spi->get_device_name(INV3_ID_ICM42688));
 #if HAL_EXTERNAL_AHRS_ENABLED
  static AP_ExternalAHRS eAHRS;
 #endif // HAL_EXTERNAL_AHRS_ENABLED
@@ -32,12 +33,12 @@ void setup(void)
     hal.console->begin(115200);
     hal.console->printf("AP_InertialSensor startup...\n");
 
-    ins._init();
+    //ins._init();
 }
 
 void loop(void)
 {
- 
+ hal.console->printf("AP_InertialSensor startup...\n");
 }
 
 const struct AP_Param::GroupInfo        GCS_MAVLINK_Parameters::var_info[] = {
