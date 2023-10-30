c05c04eba22521d0e39db6a6e42d5052f210ec8d
MikkelSPedersen <mikkel_26@fibermail.dk>
2023-10-30 13:04:41 +0100

    Adjusted Motormixing values

--- libraries/Es_9_Controllers/MotorMixing.cpp
+++ libraries/Es_9_Controllers/MotorMixing.cpp
@@ -2,10 +2,10 @@
 
 std::tuple<float, float, float, float> MotorMixing::mix(float torque_roll, float torque_pitch, float torque_yaw, float force)
 {
-    float omega_m1 = m/(4*kt) * force - Ixx/(4*kt*l_roll) * torque_roll - Iyy/(4*kt*l_pitch) * torque_pitch + Izz/(4*km) * torque_yaw;
-    float omega_m2 = m/(4*kt) * force + Ixx/(4*kt*l_roll) * torque_roll + Iyy/(4*kt*l_pitch) * torque_pitch + Izz/(4*km) * torque_yaw;
-    float omega_m3 = m/(4*kt) * force + Ixx/(4*kt*l_roll) * torque_roll - Iyy/(4*kt*l_pitch) * torque_pitch - Izz/(4*km) * torque_yaw;
-    float omega_m4 = m/(4*kt) * force - Ixx/(4*kt*l_roll) * torque_roll + Iyy/(4*kt*l_pitch) * torque_pitch - Izz/(4*km) * torque_yaw;
+    float omega_m1 = 1/(4*kt) * force - 1/(4*kt*l_roll) * torque_roll - 1/(4*kt*l_pitch) * torque_pitch + 1/(4*km) * torque_yaw;
+    float omega_m2 = 1/(4*kt) * force + 1/(4*kt*l_roll) * torque_roll + 1/(4*kt*l_pitch) * torque_pitch + 1/(4*km) * torque_yaw;
+    float omega_m3 = 1/(4*kt) * force + 1/(4*kt*l_roll) * torque_roll - 1/(4*kt*l_pitch) * torque_pitch - 1/(4*km) * torque_yaw;
+    float omega_m4 = 1/(4*kt) * force - 1/(4*kt*l_roll) * torque_roll + 1/(4*kt*l_pitch) * torque_pitch - 1/(4*km) * torque_yaw;
 
     return {omega_m1, omega_m2, omega_m3, omega_m4};
 }
